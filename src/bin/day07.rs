503432d14d7532447d62cf39828d8d5a4f9f4bdb
Olly Swanson <olly.swanson95@gmail.com>
2022-04-03 18:14:36 +0100

    Day 7: Part 1

--- src/bin/day07.rs
+++ src/bin/day07.rs
@@ -1,3 +1,15 @@
+use std::io;
+use std::io::Read;
+
+use aoc2021::day07::Crabs;
+
 fn main() -> anyhow::Result<()> {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input)?;
+
+    let crabs: Crabs = input.parse()?;
+
+    let part_1 = crabs.find_best_position().unwrap();
+    println!("Part 1: {}", part_1);
     Ok(())
 }
---
+++ src/day07.rs
@@ -0,0 +1,72 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+use anyhow::{anyhow, bail};
+use itertools::Itertools;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Crabs {
+    crabs: Vec<i32>,
+    min: i32,
+    max: i32,
+}
+
+impl Crabs {
+    /// Returns Some(total_fuel_used) or None if all the fuel is used before the crabs are in
+    /// position.
+    fn move_crabs_to_position(&self, pos: i32) -> i32 {
+        let mut fuel_used = 0;
+        for &crab_position in self.crabs.iter() {
+            fuel_used += (pos - crab_position).abs();
+        }
+        fuel_used
+    }
+
+    pub fn find_best_position(&self) -> Option<i32> {
+        (self.min..=self.max)
+            .map(|pos| self.move_crabs_to_position(pos))
+            .min()
+    }
+}
+
+impl FromStr for Crabs {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Self> {
+        let crabs: Vec<i32> = s
+            .split(',')
+            .map(|position| position.parse().map_err(|e: ParseIntError| anyhow!(e)))
+            .collect::<anyhow::Result<_>>()?;
+
+        let (&min, &max) = match crabs.iter().minmax() {
+            itertools::MinMaxResult::MinMax(min, max) => (min, max),
+            _ => bail!("Invalid input"),
+        };
+
+        Ok(Self { crabs, min, max })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_parse_crabs_fromstr() {
+        let crabs = "1,2,3,4,5,6,7,8";
+        let expected = Crabs {
+            crabs: vec![1, 2, 3, 4, 5, 6, 7, 8],
+            min: 1,
+            max: 8,
+        };
+
+        assert_eq!(crabs.parse::<Crabs>().unwrap(), expected);
+    }
+
+    #[test]
+    fn find_best_position() {
+        let crabs: Crabs = "16,1,2,0,4,2,7,1,2,14".parse().unwrap();
+
+        assert_eq!(crabs.find_best_position().unwrap(), 37);
+    }
+}
