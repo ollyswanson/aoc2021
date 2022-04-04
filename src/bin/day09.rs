d52ae83f601d50b48457506a5a8c84c75e06c420
Olly Swanson <olly.swanson95@gmail.com>
2022-04-04 23:43:57 +0100

    Day 9: Part 2
    I used a variation of the Quick Union algorithm detailed in
    "Algorithms 4th Edition" by Robert Sedgwick and Kevin Wayne to solve
    part 2.

--- src/bin/day09.rs
+++ src/bin/day09.rs
@@ -1,7 +1,7 @@
 use std::io;
 use std::io::Read;
 
-use aoc2021::day09::HeightMap;
+use aoc2021::day09::{HeightMap, QuickUnion};
 
 fn main() -> anyhow::Result<()> {
     let mut input = String::new();
@@ -11,6 +11,11 @@ fn main() -> anyhow::Result<()> {
 
     let part_1 = height_map.risk_level();
     println!("Part 1: {}", part_1);
+
+    let quick_union = QuickUnion::new(&height_map);
+
+    let part_2 = quick_union.product_three_biggest_basins();
+    println!("Part 2; {}", part_2);
 
     Ok(())
 }
--- src/day09.rs
+++ src/day09.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 
 use anyhow::anyhow;
+use itertools::Itertools;
 
 /// X and Y are the dimensions of the input with an extra layer of surrounding space
 pub struct HeightMap<const X: usize, const Y: usize> {
@@ -46,6 +47,82 @@ impl<const X: usize, const Y: usize> HeightMap<X, Y> {
     }
 }
 
+pub struct QuickUnion<'a, const X: usize, const Y: usize> {
+    height_map: &'a HeightMap<X, Y>,
+    tree_size: [[usize; X]; Y],
+    trees: [[(usize, usize); X]; Y],
+}
+
+impl<'a, const X: usize, const Y: usize> QuickUnion<'a, X, Y> {
+    pub fn new(height_map: &'a HeightMap<X, Y>) -> Self {
+        let tree_size = [[1usize; X]; Y];
+        let mut trees = [[(0, 0); X]; Y];
+
+        for y in 0..Y {
+            for x in 0..X {
+                trees[y][x] = (x, y);
+            }
+        }
+
+        let mut quick_union = Self {
+            height_map,
+            tree_size,
+            trees,
+        };
+
+        quick_union.solve();
+
+        quick_union
+    }
+
+    fn solve(&mut self) {
+        for y in 1..Y {
+            for x in 1..X {
+                self.union((x, y), (x + 1, y));
+                self.union((x, y), (x, y + 1));
+            }
+        }
+    }
+
+    fn find(&self, mut a: (usize, usize)) -> (usize, usize) {
+        while a != self.trees[a.1][a.0] {
+            a = self.trees[a.1][a.0];
+        }
+        a
+    }
+
+    fn union(&mut self, a: (usize, usize), b: (usize, usize)) {
+        if self.height_map.heights[a.1][a.0] == 9 || self.height_map.heights[b.1][b.0] == 9 {
+            return;
+        }
+
+        let root_a = self.find(a);
+        let root_b = self.find(b);
+
+        if root_a == root_b {
+            return;
+        }
+
+        if self.tree_size[root_a.1][root_a.0] < self.tree_size[root_b.1][root_b.0] {
+            self.trees[root_a.1][root_a.0] = root_b;
+            self.tree_size[root_b.1][root_b.0] += self.tree_size[root_a.1][root_a.0];
+        } else {
+            self.trees[root_b.1][root_b.0] = root_a;
+            self.tree_size[root_a.1][root_a.0] += self.tree_size[root_b.1][root_b.0];
+        }
+    }
+
+    pub fn product_three_biggest_basins(&self) -> usize {
+        self.tree_size
+            .iter()
+            .flatten()
+            .sorted()
+            .rev()
+            .take(3)
+            .product()
+    }
+}
+
 impl<const X: usize, const Y: usize> FromStr for HeightMap<X, Y> {
     type Err = anyhow::Error;
 
@@ -64,6 +141,8 @@ impl<const X: usize, const Y: usize> FromStr for HeightMap<X, Y> {
 
 #[cfg(test)]
 mod tests {
+    use itertools::Itertools;
+
     use super::*;
 
     #[test]
@@ -78,5 +157,19 @@ mod tests {
         let height_map: HeightMap<12, 7> = height_map.parse().unwrap();
 
         assert_eq!(height_map.risk_level(), 15)
+    }
+
+    #[test]
+    fn part_2() {
+        let height_map = "\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+        let height_map: HeightMap<12, 7> = height_map.parse().unwrap();
+        let quick_union = QuickUnion::new(&height_map);
+
+        assert_eq!(quick_union.product_three_biggest_basins(), 1134);
     }
 }
