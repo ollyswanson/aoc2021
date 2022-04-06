ba888017da7ac4e64e2417bb2dc4542f8a666538
Olly Swanson <olly.swanson95@gmail.com>
2022-04-06 19:33:31 +0100

    Day 13: Part 2
    Print result of folds to stdout. Result was:
    
    ###..#....#..#.####...##.###....##.####
    #..#.#....#..#.#.......#.#..#....#.#...
    ###..#....####.###.....#.#..#....#.###.
    #..#.#....#..#.#.......#.###.....#.#...
    #..#.#....#..#.#....#..#.#....#..#.#...
    ###..####.#..#.#.....##..#.....##..#...

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -3,9 +3,13 @@ use aoc2021::day13::parse_input;
 fn main() -> anyhow::Result<()> {
     let input = include_str!("../../inputs/day13.txt");
     let (mut paper, folds) = parse_input(input)?;
-    paper.fold(folds[0]);
+    paper.fold(&folds[0..1]);
 
     let part_1 = paper.count_points();
     println!("Part 1: {}", part_1);
+
+    paper.fold(&folds[1..]);
+    print!("{}", paper);
+
     Ok(())
 }
--- src/day13.rs
+++ src/day13.rs
@@ -1,38 +1,74 @@
 use std::collections::HashSet;
+use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 
 use anyhow::{anyhow, bail};
+use itertools::{Itertools, MinMaxResult};
 
 #[derive(Debug, Clone)]
 pub struct Paper(HashSet<(i32, i32)>);
 
 impl Paper {
-    pub fn fold(&mut self, fold: Fold) {
-        let current = std::mem::take(&mut self.0);
-
-        for (x, y) in current {
-            let (x, y) = match fold {
-                Fold::X(offset) => {
-                    if x > offset {
-                        (2 * offset - x, y)
-                    } else {
-                        (x, y)
+    pub fn fold(&mut self, folds: &[Fold]) {
+        for fold in folds {
+            let current = std::mem::take(&mut self.0);
+            for (x, y) in current {
+                let (x, y) = match fold {
+                    Fold::X(offset) => {
+                        if x > *offset {
+                            (2 * offset - x, y)
+                        } else {
+                            (x, y)
+                        }
                     }
-                }
-                Fold::Y(offset) => {
-                    if y > offset {
-                        (x, 2 * offset - y)
-                    } else {
-                        (x, y)
+                    Fold::Y(offset) => {
+                        if y > *offset {
+                            (x, 2 * offset - y)
+                        } else {
+                            (x, y)
+                        }
                     }
-                }
-            };
-            self.0.insert((x, y));
+                };
+                self.0.insert((x, y));
+            }
         }
     }
 
     pub fn count_points(&self) -> usize {
         self.0.len()
+    }
+}
+
+impl Display for Paper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let (x_min, x_max) =
+            if let MinMaxResult::MinMax(x_min, x_max) = self.0.iter().map(|(x, _)| x).minmax() {
+                (x_min, x_max)
+            } else {
+                return Err(std::fmt::Error);
+            };
+        let (y_min, y_max) =
+            if let MinMaxResult::MinMax(y_min, y_max) = self.0.iter().map(|(_, y)| y).minmax() {
+                (y_min, y_max)
+            } else {
+                return Err(std::fmt::Error);
+            };
+
+        let dx = (x_max - x_min) as usize;
+        let dy = (y_max - y_min) as usize;
+
+        let mut grid = vec![vec!['.'; dx + 1]; dy + 1];
+        for &(x, y) in self.0.iter() {
+            let x = (x - x_min) as usize;
+            let y = (y - y_min) as usize;
+            grid[y][x] = '#';
+        }
+
+        for line in grid.into_iter().map(|line| line.iter().collect::<String>()) {
+            writeln!(f, "{}", line)?;
+        }
+
+        Ok(())
     }
 }
 
@@ -123,7 +159,7 @@ fold along y=7
 fold along x=5";
 
         let (mut paper, folds) = parse_input(input).unwrap();
-        paper.fold(folds[0]);
+        paper.fold(&folds[0..1]);
 
         assert_eq!(paper.count_points(), 17);
     }
